73f772023f44af4dc044de26fa035414ccfa5ec5
Aishwary Kumar <aishwary08@gmail.com>
2024-08-06 02:51:11 +0530

    Fix: calculator is defined

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -1,4 +1,12 @@
+const Calculator = require('../calculator')
+
 describe('String Calculator', () => {
+    let calculator;
+
+    beforeEach(() => {
+        calculator = new Calculator();
+    });
+
     it('handles basic cases', () => {
         expect(calculator.add("")).toBe(0);
     });
