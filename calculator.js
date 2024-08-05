236d9a93b9a2e021eabee19a385656d29ddb13d1
Aishwary Kumar <aishwary08@gmail.com>
2024-08-06 03:14:55 +0530

    Fix: custom delimiter support

--- calculator.js
+++ calculator.js
@@ -1,7 +1,16 @@
 class Calculator {
 
     add = (numbers) => {
-        const parts = numbers.split(/[\n,]/)
+        let delimiter = /[\n,]/;
+        let numberString = numbers;
+
+        if (numbers.startsWith('//')) {
+            const delimiterEndIndex = numbers.indexOf('\n');
+            delimiter = new RegExp(numbers.substring(2, delimiterEndIndex));
+            numberString = numbers.substring(delimiterEndIndex + 1);
+        }
+
+        const parts = numberString.split(delimiter);
         let sum = 0;
 
         parts.forEach(part => {
