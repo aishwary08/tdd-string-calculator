7e33194cb305506e17e1db472ac2b0f0e8d3f098
Aishwary Kumar <aishwary08@gmail.com>
2024-08-06 10:40:19 +0530

    Modularized and added documentation

--- calculator.js
+++ calculator.js
@@ -1,6 +1,23 @@
 class Calculator {
-
+    /**
+     * Add numbers in a string, using specified delimiters.
+     * @param {string} numbers - The string containing numbers to be added.
+     * @returns {number} The sum of the numbers.
+     * @throws Will throw an error if there are negative numbers.
+     */
     add = (numbers) => {
+        let { delimiter, numberString } = this.extractDelimiter(numbers);
+        let parts = this.splitNumbers(numberString, delimiter);
+        this.checkForNegatives(parts);
+        return this.calculateSum(parts);
+    }
+
+    /**
+     * Extracts the delimiter and the number string from the input.
+     * @param {string} numbers - The input string containing the delimiter and numbers.
+     * @returns {Object} An object containing the delimiter and number string.
+     */
+    extractDelimiter = (numbers) => {
         let delimiter = /[\n,]/;
         let numberString = numbers;
 
@@ -20,22 +37,44 @@ class Calculator {
             numberString = numbers.substring(delimiterEndIndex + 1);
         }
 
-        const parts = numberString.split(delimiter);
-        const negatives = parts.filter(num => parseInt(num) < 0);
+        return { delimiter, numberString };
+    }
 
+    /**
+     * Splits the number string using the given delimiter.
+     * @param {string} numberString - The string containing numbers.
+     * @param {RegExp} delimiter - The delimiter used for splitting the numbers.
+     * @returns {Array<string>} An array of split numbers as strings.
+     */
+    splitNumbers = (numberString, delimiter) => {
+        return numberString.split(delimiter);
+    }
+
+    /**
+     * Checks for negative numbers in the parts array.
+     * @param {Array<string>} parts - The array of split numbers as strings.
+     * @throws Will throw an error if there are negative numbers.
+     */
+    checkForNegatives = (parts) => {
+        const negatives = parts.filter(num => parseInt(num) < 0);
         if (negatives.length > 0) {
             throw new Error(`negative numbers not allowed: ${negatives.join(',')}`);
         }
+    }
 
+    /**
+     * Calculates the sum of the numbers in the parts array, ignoring numbers greater than 1000.
+     * @param {Array<string>} parts - The array of split numbers as strings.
+     * @returns {number} The sum of the numbers.
+     */
+    calculateSum = (parts) => {
         let sum = 0;
-
         parts.forEach(part => {
             const value = parseInt(part || 0);
             if (value <= 1000) {
                 sum += value
             }
         })
-
         return sum
     }
 }
